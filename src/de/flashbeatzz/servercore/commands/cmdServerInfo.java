6f038ee854c630fee76cdf0ee90bb29f1bde8bc3
LayZz <niclas.klein@yahoo.de>
2016-03-31 14:32:41 +0200

    ~ fixes

--- src/de/flashbeatzz/servercore/commands/cmdServerInfo.java
+++ src/de/flashbeatzz/servercore/commands/cmdServerInfo.java
@@ -4,42 +4,39 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryUsage;
+
 public class cmdServerInfo implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender cs, Command cmd, String lable, String[] args) {
 
-        if(args.length == 0) {
-            Runtime runtime = Runtime.getRuntime();
+        if (args.length == 0) {
+            /*Runtime runtime = Runtime.getRuntime();
             long allocatedRAM = runtime.totalMemory();
-            long maxRAM = runtime.maxMemory()/1024;
+            long maxRAM = runtime.maxMemory() / 1024;
             long freeRAM = runtime.freeMemory();
+            long usedRAM = maxRAM - freeRAM;*/
+            MemoryUsage heapMemoryUsage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+            long maxRAM = heapMemoryUsage.getMax();
+            long freeRAM = heapMemoryUsage.getUsed();
             long usedRAM = maxRAM - freeRAM;
             String os = System.getProperty("os.name");
             String os_version = System.getProperty("os.version");
+            Integer gb = 1024*1024*1024;
             cs.sendMessage("§8§l[]======>> §6§lSERVER - INFO §8§l<<======[]\n" +
                     "§dBetriebssystem:  §f" + os + "\n" +
                     "§dVersion:         §f" + os_version + "\n" +
                     "\n" +
-                    "§dZugeteilter RAM: §f" + toGB(allocatedRAM) + " GB\n" +
-                    "§dMaximaler RAM:   §f" + toGB(maxRAM) + " GB\n" +
-                    "§dBenutzter RAM:   §f" + toGB(usedRAM) + " GB\n" +
-                    "§dFreier RAM:      §f" + toGB(freeRAM) + " GB\n" +
+                    "§dMaximaler RAM:   §f" + maxRAM / gb + " GB\n" +
+                    "§dBenutzter RAM:   §f" + usedRAM / gb + " GB\n" +
+                    "§dFreier RAM:      §f" + freeRAM / gb + " GB\n" +
                     "§8§l[]======>> §6§lSERVER - INFO §8§l<<======[]");
             return true;
         }
         cs.sendMessage("§cFalsche Verwendung: /serverinfo");
         return true;
-    }
-
-    public static String toGB(long bytes) {
-        int u = 0;
-        for (;bytes > 1024*1024; bytes >>= 10) {
-            u++;
-        }
-        if (bytes > 1024)
-            u++;
-        return String.format("%.1f %cB", bytes/1024f, " kMGTPE".charAt(u));
     }
 
 }
